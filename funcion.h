2de6bc3cc1c20b210798ee8328e9d220ad0e8ff2
Lucio alumno (thefenix) <111651430+elfenix-67@users.noreply.github.com>
2024-11-29 14:03:57 -0500

    Create funcion.h

---
+++ funcion.h
@@ -0,0 +1,5 @@
+#ifndef FUNCION_H
+#define FUNCION_H
+char(Contar_caracteres);
+void palabras(char *texto,char *longitud_alta, char *palabra_larga, char *palabra_corta, char *logitud_corta);
+#endif

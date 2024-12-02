ad96a3395e29ff32c63c41ce8f2ef332fa5ee730
Lucio alumno (thefenix) <111651430+elfenix-67@users.noreply.github.com>
2024-12-01 20:48:39 -0500

    Update programa.c

--- programa.c
+++ programa.c
@@ -1,36 +1,32 @@
 #include <stdio.h>
 #include <string.h>
 #include "funcion.h"
-void palabras(char *texto,char **longitud_alta, char *palabra_larga, char *palabra_corta, char *logitud_corta){
-     int i = 0, start = 0, len = 0;
-    *longitud_alta = 0;
-    *longitud_corta = 1000;
-    while (texto[i] != '\0'){
-        if (texto [i] == ' ') {
+
+void palabras(const char *texto, char *palabra_larga, char *palabra_corta, int *longitud_larga, int *longitud_corta) {
+    int i = 0, start = 0, len = 0;
+    *longitud_larga = 0;
+    *longitud_corta = 1000; 
+    while (texto[i] != '\0') {
+        if (texto[i] == ' ') {
             i++;
             start = i;
             continue;
         }
-        len=0;
-        while (texto[i] != ' ' && texto[i] != '\0'){
-        len++;
-        i++;
+        len = 0;
+        while (texto[i] != ' ' && texto[i] != '\0') {
+            len++;
+            i++;
         }
-        if (len > *longitud_alta){
-            *longitud_alta = len;
-            for (int j = 0; j< len; j++) {
-            }
-            longitud_alta[len]='\0';
+        if (len > *longitud_larga) {
+            *longitud_larga = len;
+            strncpy(palabra_larga, texto + start, len);
+            palabra_larga[len] = '\0';
         }
-        if (len < *logitud_corta){
-            *logitud_corta = len;
-            for (int j = 0; j < len; j++){
-                palabra_corta[j] = texto[start + j];
-            }
-            logitud_corta[len] = '\0';
+        if (len < *longitud_corta) {
+            *longitud_corta = len;
+            strncpy(palabra_corta, texto + start, len);
+            palabra_corta[len] = '\0'; 
         }
-        start = i;
-        
+        start = i; 
     }
 }
-

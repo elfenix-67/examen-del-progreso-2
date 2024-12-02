80e7033c47669a228cea13bd3a4b328d9fdf356a
Lucio alumno (thefenix) <111651430+elfenix-67@users.noreply.github.com>
2024-12-01 20:47:44 -0500

    Update funcion.h

--- funcion.h
+++ funcion.h
@@ -1,5 +1,4 @@
 #ifndef FUNCION_H
 #define FUNCION_H
-char(Contar_caracteres);
-void palabras(char *texto,char *longitud_alta, char *palabra_larga, char *palabra_corta, char *logitud_corta);
-#endif
+void palabras(const char *texto, char *palabra_larga, char *palabra_corta, int *longitud_larga, int *longitud_corta);
+#endif 

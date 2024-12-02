95a2114956e06d2ee76563ddcb0484987a0c39ed
Lucio alumno (thefenix) <111651430+elfenix-67@users.noreply.github.com>
2024-12-01 20:48:19 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 #include "funcion.h"
-int main(){
+
+int main() {
     char texto[100];
-    char *palabra_larga[100], *palabra_corta[100];
-    int longitud_alta = 0, longitud_corta = 0;
-    printf("ingrese un texto:");
+    char palabra_larga[100], palabra_corta[100];
+    int longitud_larga = 0, longitud_corta = 0;
+    printf("Ingrese un texto: ");
     scanf("%[^\n]", texto);
-    palabras(texto, &longitud_alta, &longitud_corta, palabra_larga, palabra_corta);
-    printf("la palabra con mas caracteres es: %s (%d caracteres)\n"), longitud_alta, palabra_larga;
-    printf("la palabra con menos caracteres es:%s  (%d caracteres)\n"), longitud_corta, palabra_corta;
-    
-return 0;
+    palabras(texto, palabra_larga, palabra_corta, &longitud_larga, &longitud_corta);
+    printf("La palabra con mas caracteres es: '%s' (%d caracteres)\n", palabra_larga, longitud_larga);
+    printf("La palabra con menos caracteres es: '%s' (%d caracteres)\n", palabra_corta, longitud_corta);
+
+    return 0;
 }
